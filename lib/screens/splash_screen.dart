d6124de62c3c30abb2c8986b1985a6587438c705
Mustafa Ahmed <109815112+MustafaAhmed313@users.noreply.github.com>
2025-02-14 19:03:10 +0200

    modify image asset of android

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -30,7 +30,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   void initState() {
-    Future.delayed(Duration(seconds: 5), () => {
+    Future.delayed(Duration(seconds: 10), () => {
       Get.offAll(OnboardingScreen())
     });
     super.initState();
