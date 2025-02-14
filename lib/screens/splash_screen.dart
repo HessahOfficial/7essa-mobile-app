d02a2e3472e57756f80b552da82a99e5da8a794e
Mustafa Ahmed <109815112+MustafaAhmed313@users.noreply.github.com>
2025-02-14 18:26:17 +0200

    UI Screen: Add Splash Screen
    - add logo image.
    - installing getx library for routing control.
    - use `Future.delayed()` to delay the splash screen.

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hessa/screens/onboarding_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Center(
+            child: Image(image: AssetImage('assets/images/Logo.png')),
+          ),
+        )
+    );
+  }
+
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 5), () => {
+      Get.offAll(OnboardingScreen())
+    });
+    super.initState();
+  }
+}
+
