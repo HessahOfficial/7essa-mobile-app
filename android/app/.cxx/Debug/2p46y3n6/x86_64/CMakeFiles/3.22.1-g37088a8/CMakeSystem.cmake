88670d3687dd67308aa7e3fd6bc685c50197ce67
MustafaAhmed3132004 <mostafa3132004@gmail.com>
2025-05-20 15:06:14 +0300

    remove the project

--- android/app/.cxx/Debug/2p46y3n6/x86_64/CMakeFiles/3.22.1-g37088a8/CMakeSystem.cmake
+++ android/app/.cxx/Debug/2p46y3n6/x86_64/CMakeFiles/3.22.1-g37088a8/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Linux-6.1.0-32-amd64")
+set(CMAKE_HOST_SYSTEM "Linux-6.1.0-33-amd64")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "6.1.0-32-amd64")
+set(CMAKE_HOST_SYSTEM_VERSION "6.1.0-33-amd64")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/home/meow/Android/Sdk/ndk/26.3.11579264/build/cmake/android.toolchain.cmake")
